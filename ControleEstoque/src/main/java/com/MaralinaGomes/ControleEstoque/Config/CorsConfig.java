5a8c895f87200ec8e2649b06da821b88c7b3e904
Davi Gomes Pinto Coelho <100974717+davigpc@users.noreply.github.com>
2025-08-14 21:33:32 -0300

    Update CorsConfig.java

--- ControleEstoque/src/main/java/com/MaralinaGomes/ControleEstoque/Config/CorsConfig.java
+++ ControleEstoque/src/main/java/com/MaralinaGomes/ControleEstoque/Config/CorsConfig.java
@@ -1,15 +1,22 @@
 package com.MaralinaGomes.ControleEstoque.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    // Injeta o valor da variável de ambiente "ALLOWED_ORIGIN" definida no render.yaml
+    @Value("${ALLOWED_ORIGIN}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Libera os endpoints sob /api/
-                .allowedOrigins("http://localhost:5173") // A porta padrão do Vite/React
+        registry.addMapping("/api/**")
+                // Usa a variável para permitir a origem do frontend na Render
+                .allowedOrigins(allowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
